cee01af60a9e8f47deb8fc540e8eff07b886e765
zflbigbang <2759025860@qq.com>
2024-03-23 11:29:32 +0800

    up yaml

--- src/main/java/cn/hzcu/timeback/TimebackApplication.java
+++ src/main/java/cn/hzcu/timeback/TimebackApplication.java
@@ -1,9 +1,11 @@
 package cn.hzcu.timeback;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan
 public class TimebackApplication {
 
 	public static void main(String[] args) {
